79ffa6a10f39101da8fbbf89e5232a3f15b2dc33
amadorcarlosa <75299545+amadorcarlosa@users.noreply.github.com>
2020-12-07 01:17:47 -0600

    Add files via upload

---
+++ PointstoWin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointstoWin : MonoBehaviour
+{  
+    private int currentPoints;
+    private int pointsToWin;
+    public GameObject rods;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsToWin = rods.transform.childCount;
+        print("Points to Win  " + pointsToWin);
+    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentPoints >= pointsToWin)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+    public void AddPoints()
+    {
+        currentPoints++;
+    }
+}
---
+++ RODS12220.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RODS12220 : MonoBehaviour
+{
+    public GameObject Name;
+    public bool Marker;
+
+
+    private bool moving2;
+    private bool finished;
+
+
+    private string pop;
+
+
+
+    private float startPosX2;
+    private float startPosY2;
+    private float DifferenceofX;
+    private float DifferenceofY;
+
+    private Vector3 resetPosition2;
+    void Start()
+    {
+
+        resetPosition2 = this.transform.localPosition;
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (finished == false)
+        {
+            if (moving2)
+            {
+                Vector3 mousePos;
+                mousePos = Input.mousePosition;
+                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+                gameObject.transform.position = new Vector3(mousePos.x - startPosX2, mousePos.y - startPosY2, gameObject.transform.position.z);
+
+
+            }
+
+        }
+      
+
+    }
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX2 = mousePos.x - transform.position.x;
+            startPosY2 = mousePos.y - transform.position.y;
+
+            moving2 = true;
+        }
+    }
+    private void OnMouseUp()
+    {
+        moving2 = false;
+
+       
+        pop = tag;
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag(pop);
+     
+
+        Debug.Log("update");
+
+        foreach (GameObject go in gos)
+        {
+
+
+
+
+            float curDistancex = (Mathf.Abs(go.transform.position.x - this.transform.position.x));
+           float curDistancey = (Mathf.Abs(go.transform.position.y - this.transform.position.y));
+
+            if (curDistancex <= 0.2f && curDistancey <= 0.2f && go.name != name && this.gameObject.layer != go.layer)
+            {
+                print("name" + name + "New Position" + go.transform.position);
+                this.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, go.transform.position.z);
+                go.SetActive(false);
+                finished = true;
+                
+               break;
+                
+            }
+            else
+               
+            {
+
+            }
+        }
+        if (finished == false)
+        {
+            this.transform.localPosition = new Vector3(resetPosition2.x, resetPosition2.y, resetPosition2.z);
+        }
+
+    }
+   
+}
+    
+   
+
+ 
